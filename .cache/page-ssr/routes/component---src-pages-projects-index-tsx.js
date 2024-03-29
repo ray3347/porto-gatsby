f8e13a39a90ec4dca5915f21b561b3b02a39ce56
ray3347 <ansel.knb@gmail.com>
2024-03-29 11:07:39 +0700

    added cache

---
+++ .cache/page-ssr/routes/component---src-pages-projects-index-tsx.js
@@ -0,0 +1,28 @@
+"use strict";
+exports.id = "component---src-pages-projects-index-tsx";
+exports.ids = ["component---src-pages-projects-index-tsx"];
+exports.modules = {
+
+/***/ "./src/pages/projects/index.tsx?export=default":
+/*!*****************************************************!*\
+  !*** ./src/pages/projects/index.tsx?export=default ***!
+  \*****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+function Projects() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("main", null, "ppp");
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Projects);
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-projects-index-tsx.js.map
---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\user\\Documents\\Code Projects\\Personal Web Revamp\\porto-website\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-tsx": preferDefault(require("C:\\Users\\user\\Documents\\Code Projects\\Personal Web Revamp\\porto-website\\src\\pages\\404.tsx")),
+  "component---src-pages-index-tsx": preferDefault(require("C:\\Users\\user\\Documents\\Code Projects\\Personal Web Revamp\\porto-website\\src\\pages\\index.tsx")),
+  "component---src-pages-projects-index-tsx": preferDefault(require("C:\\Users\\user\\Documents\\Code Projects\\Personal Web Revamp\\porto-website\\src\\pages\\projects\\index.tsx"))
+}
+
